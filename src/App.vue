932e00544cc11c394eb0bead1deec00353d9d7a7
Ariez Flores <aries.flores@gmail.com>
2023-07-06 14:56:09 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex items-center justify-center">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" alt="Vite logo" class="h-40 mt-20 mr-20"/>
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" alt="Vue logo" class="h-40 mr-20 mt-20"/>
+    </a>
+    <a href="https://www.typescriptlang.org/" target="_blank">
+      <img src="./assets/typescript.svg" alt="Typescript logo" class="h-40 mr-20 mt-20"/>
+    </a>
+    <a href="https://tailwindcss.com/" target="_blank">
+      <img src="./assets/tailwindcss.png" alt="TaiwindCSS logo" class="h-40 mt-20"/>
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue + Typescript + TailwindCSS" />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+</script>
+
+<template>
+  <div class="flex items-center justify-center mt-40">
+    <h1 class="text-2xl font-bold">{{ msg }}</h1>
+  </div>
+</template>
